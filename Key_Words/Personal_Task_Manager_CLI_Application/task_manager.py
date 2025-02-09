7afb6fd952968cc2fbac7bc93b891752befcb87e
Kartik Kakodiya <kart00ik@gmail.com>
2025-02-09 10:21:22 +0530

    modified def

--- Key_Words/Personal_Task_Manager_CLI_Application/task_manager.py
+++ Key_Words/Personal_Task_Manager_CLI_Application/task_manager.py
@@ -1,5 +1,6 @@
 # task_manager.py
 
+# Step 1: Set Up the Project
 class Task:
     def __init__(self, name, due_date=None):
         self.name = name
@@ -9,6 +10,8 @@ class Task:
         return f"{self.name} (Due: {self.due_date})"
 
 tasks = []  # List to store tasks
+
+# Step 2: Implement Core Functions
 
 def add_task(name, due_date=None):
     """Add a new task to the list."""
@@ -29,5 +32,31 @@ def delete_task(task_id):
     try:
         task = task.pop(task_id - 1)
         print(f"Deleted task: {task}")
-    except IndexError:
+    except IndexError: 
         print("Invalid task ID.")
+
+def saving_in_file():
+   try:
+    with open('tasks.txt',"w") as file:
+       for task in tasks:
+        file.write(f'{task.name}, {task.due_date}/n')
+    print("Tasks saved successfully.")
+   except Exception as e:
+        print(f"Error saving tasks:{e}")
+
+def load_tasks():
+    try:
+        with open('tasks.txt','r') as file:
+            for line in file:
+                name,due_date = line.strip().split(',')
+                tasks.append(Task(name,due_date if due_date else None))
+        print("Tasks loaded successfully.")
+
+    except FileNotFoundError:
+        print("No saved tasks found")
+    except Exception as e:
+        print(f"Error loading tasks: {e}")
+
+
+# Step 3: Create the Main Menu
+
